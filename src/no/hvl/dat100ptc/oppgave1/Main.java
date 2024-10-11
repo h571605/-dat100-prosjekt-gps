027a354eb4d2d7f6a414bb44f1ff4fae0ffffe7a
h571605 <571605@stud.hvl.no>
2024-10-11 13:39:15 +0200

    fullførte oppgave 1

--- src/no/hvl/dat100ptc/oppgave1/Main.java
+++ src/no/hvl/dat100ptc/oppgave1/Main.java
@@ -3,9 +3,12 @@ package no.hvl.dat100ptc.oppgave1;
 public class Main {
 
 	public static void main(String[] args) {
+		GPSPoint myObj = new GPSPoint(1, 2.0, 3.0, 5.0);
+		System.out.println(myObj.getTime());
+		myObj.setTime(2);
+		System.out.println(myObj.getTime());
 		
-		// TODO
-		
+		System.out.println(myObj.toString());
 	}
 
 }
